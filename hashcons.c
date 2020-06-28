11cfd1d49f3cede267dca4d0e524edef3a2c049c
Amir <hesamian@uwm.edu>
2020-06-27 22:21:29 -0500

    added more comments

--- hashcons.c
+++ hashcons.c
@@ -6,12 +6,24 @@
 #define HC_INITIAL_BASE_SIZE 61
 #define MAX_DENSITY 0.5
 
+/**
+ * Initializes a table
+ * @param hc table
+ * @param capacity new capacity
+ */
 void hc_initialize(HASH_CONS_TABLE hc, const int capacity) {
     hc->capacity = capacity;
     hc->table = calloc(hc->capacity, sizeof(void *));
     hc->size = 0;
 }
 
+/**
+ * Finds the candidate index intended to get inserted or searched in table
+ * @param hc table
+ * @param item the item looking to be added or removed
+ * @param insert_mode true indicates insert false indicates search
+ * @return
+ */
 static int hc_candidate_index(HASH_CONS_TABLE hc, void *item, bool insert_mode) {
     int attempt = 0;
     int hash = hc->hashf(item);
@@ -34,13 +46,24 @@ static int hc_candidate_index(HASH_CONS_TABLE hc, void *item, bool insert_mode) 
     return -1;
 }
 
-static void *hc_insert(HASH_CONS_TABLE hc, void *item) {
+/**
+ * Insert an item into table
+ * @param hc table
+ * @param item the item intended to get inserted into the table
+ */
+static void hc_insert(HASH_CONS_TABLE hc, void *item) {
     int index = hc_candidate_index(hc, item, true);
 
     hc->table[index] = item;
     hc->size++;
 }
 
+/**
+ * Search an item in table
+ * @param hc table
+ * @param item the item intended to get searched in the table
+ * @return the item or null
+ */
 static void *hc_search(HASH_CONS_TABLE hc, void *item) {
     int index = hc_candidate_index(hc, item, false);
 
@@ -62,6 +85,13 @@ static void hc_resize(HASH_CONS_TABLE hc, const int capacity) {
     free(old_table);
 }
 
+/**
+ * Insert an item into table if item is not already in table or just returns the existing item
+ * @param item the item
+ * @param temp_size item size
+ * @param hc table
+ * @return item just got inserted into the table or existing item
+ */
 void *hash_cons_get(void *item, size_t temp_size, HASH_CONS_TABLE hc) {
     void *result;
 
