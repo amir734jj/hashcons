d608e3c4204366cb28f9dcbb5ca4724eba53d805
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2020-11-23 00:34:34 -0600

    Update hashcons.h

--- hashcons.h
+++ hashcons.h
@@ -1,19 +1,19 @@
 #ifndef HASHCONS_H
 #define HASHCONS_H
 
-#include <stddef.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 typedef int (*Hash_Cons_Hash)(void *);
 typedef bool (*Hash_Cons_Equal)(void *, void *);
 
 typedef struct hash_cons_table {
-    int size;
-    int capacity;
-    void **table;
-    Hash_Cons_Hash hashf;
-    Hash_Cons_Equal equalf;
-} *HASH_CONS_TABLE;
+  Hash_Cons_Hash hashf;
+  Hash_Cons_Equal equalf;
+  int size;
+  int capacity;
+  void **table;
+} * HASH_CONS_TABLE;
 
 /**
  * Get item if there is one otherwise create one
@@ -22,5 +22,20 @@ typedef struct hash_cons_table {
  * @param hashcons table
  */
 void *hash_cons_get(void *temp_item, size_t temp_size, HASH_CONS_TABLE table);
+
+/**
+ * Hash string
+ * @param string
+ * @return intger hash value
+ */
+int hash_string(char *str);
+
+/**
+ * Combine two hash values into one
+ * @param hash1
+ * @param hash2
+ * @return combined hash 
+ */
+int hash_mix(int h1, int h2);
 
 #endif
